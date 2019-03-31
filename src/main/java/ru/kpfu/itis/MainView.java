2122cf98a5cdd3e85d316a24a022a53e821b290c
Enoxus <reqenoxus@gmail.com>
2019-03-31 09:08:21 +0300

    initial commit

---
+++ src/main/java/ru/kpfu/itis/MainView.java
@@ -0,0 +1,17 @@
+package ru.kpfu.itis;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route("")
+@PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+
+    }
+}
