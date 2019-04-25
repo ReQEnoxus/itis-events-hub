f8d29547355a6f28cfb9163790cafe1f109b0a04
enoxus <reqenoxus@gmail.com>
2019-04-25 18:47:50 +0300

    added back button

--- src/main/java/ru/kpfu/itis/view/modal/LoginWindow.java
+++ src/main/java/ru/kpfu/itis/view/modal/LoginWindow.java
@@ -1,14 +1,40 @@
 package ru.kpfu.itis.view.modal;
 
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import ru.kpfu.itis.auth.AuthManager;
 
 public class LoginWindow {
     public static void show() {
         LoginOverlay loginOverlay = new LoginOverlay();
-        loginOverlay.setTitle("ITIS Events Hub");
+
+        VerticalLayout titleLayout = new VerticalLayout();
+        H1 title = new H1("ITIS Events HUB");
+        title.getStyle().set("color", "#FFFFFF");
+        title.getStyle().set("position", "relative");
+        title.getStyle().set("right", "17px");
+        title.getStyle().set("bottom", "50px");
+        title.getStyle().set("width", "300px");
+        Button exitButton = new Button(VaadinIcon.CLOSE.create());
+        exitButton.addClickShortcut(Key.ESCAPE);
+        exitButton.getStyle().set("color", "#FFFFFF");
+        exitButton.getStyle().set("position", "relative");
+        exitButton.getStyle().set("bottom", "35px");
+        exitButton.getStyle().set("right", "35px");
+        exitButton.addClickListener(evt -> loginOverlay.close());
+        titleLayout.getStyle().set("height", "100px");
+        titleLayout.add(exitButton);
+        titleLayout.add(title);
+        titleLayout.setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.START);
+
+        loginOverlay.setTitle(titleLayout);
         loginOverlay.setDescription("Войдите в систему");
         loginOverlay.setForgotPasswordButtonVisible(true);
 
