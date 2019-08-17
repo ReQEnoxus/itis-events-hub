d80220418cf41aee9d3da4fb1d7bbcfbf98fbd5d
enoxus <reqenoxus@gmail.com>
2019-08-17 17:42:41 +0300

    added custom servlet

--- src/main/java/ru/kpfu/itis/service/api/EventServlet.java
+++ src/main/java/ru/kpfu/itis/service/api/EventServlet.java
@@ -3,19 +3,19 @@ package ru.kpfu.itis.service.api;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.vaadin.flow.server.VaadinServlet;
 import ru.kpfu.itis.entity.Event;
 import ru.kpfu.itis.service.EventService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
 @WebServlet(urlPatterns = "/events", name = "EventServlet")
-public class EventServlet extends HttpServlet {
+public class EventServlet extends VaadinServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<Event> events;
